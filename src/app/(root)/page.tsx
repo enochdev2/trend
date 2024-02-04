43c6e420e1af3a39c154812756c715b0b93aa6a8
enoch-promise <enochpromise.va@gmail.com>
2024-02-03 20:31:07 -0800

    commit

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,34 +1,34 @@
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-// import ThreadCard from "@/components/cards/ThreadCard";
+import ThreadCard from "@/components/cards/ThreadCard";
 // import Pagination from "@/components/shared/Pagination";
 
-// import { fetchPosts } from "@/lib/actions/thread.actions";
-// import { fetchUser } from "@/lib/actions/user.actions";
+import { fetchPosts } from "@/lib/actions/thread.actions";
+import { fetchUser } from "@/lib/actions/user.actions";
 
 async function Home({
   searchParams,
 }: {
   searchParams: { [key: string]: string | undefined };
 }) {
-  // const user = await currentUser();
-  // if (!user) return null;
+  const user = await currentUser();
+  if (!user) return null;
 
-  // const userInfo = await fetchUser(user.id);
-  // if (!userInfo?.onboarded) redirect("/onboarding");
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
 
-  // const result = await fetchPosts(
-  //   searchParams.page ? +searchParams.page : 1,
-  //   30
-  // );
+  const result = await fetchPosts(
+    searchParams.page ? +searchParams.page : 1,
+    30
+  );
 
   return (
     <>
       <h1 className='head-text text-left'>Home</h1>
 
       <section className='mt-9 flex flex-col gap-10'>
-        {/* {result.posts.length === 0 ? (
+        {result.posts.length === 0 ? (
           <p className='no-result'>No threads found</p>
         ) : (
           <>
@@ -46,7 +46,7 @@ async function Home({
               />
             ))}
           </>
-        )} */}
+        )}
       </section>
 
       {/* <Pagination
