68ee0ee81d4f89e68cb913c3a98392d2a0b06ceb
enoch-promise <enochpromise.va@gmail.com>
2024-02-04 23:48:04 -0800

    Commit

--- src/components/RightSidebar.tsx
+++ src/components/RightSidebar.tsx
@@ -1,20 +1,20 @@
 import { currentUser } from "@clerk/nextjs";
 
-// import UserCard from "../cards/UserCard";
+import UserCard from "./cards/UserCard";
 
-// import { fetchCommunities } from "@/lib/actions/community.actions";
-// import { fetchUsers } from "@/lib/actions/user.actions";
+import { fetchCommunities } from "@/lib/actions/community.actions";
+import { fetchUsers } from "@/lib/actions/user.actions";
 
 async function RightSidebar() {
-  // const user = await currentUser();
-  // if (!user) return null;
+  const user = await currentUser();
+  if (!user) return null;
 
-  // const similarMinds = await fetchUsers({
-  //   userId: user.id,
-  //   pageSize: 4,
-  // });
+  const similarMinds = await fetchUsers({
+    userId: user.id,
+    pageSize: 4,
+  });
 
-  // const suggestedCOmmunities = await fetchCommunities({ pageSize: 4 });
+  const suggestedCOmmunities = await fetchCommunities({ pageSize: 4 });
 
   return (
     <section className='custom-scrollbar rightsidebar'>
@@ -24,7 +24,7 @@ async function RightSidebar() {
         </h3>
 
         <div className='mt-7 flex w-[350px] flex-col gap-9'>
-          {/* {suggestedCOmmunities.communities.length > 0 ? (
+          {suggestedCOmmunities.communities.length > 0 ? (
             <>
               {suggestedCOmmunities.communities.map((community) => (
                 <UserCard
@@ -41,7 +41,7 @@ async function RightSidebar() {
             <p className='!text-base-regular text-light-3'>
               No communities yet
             </p>
-          )} */}
+          )}
           <p className='!text-base-regular text-light-3'>
             No communities yet
           </p>
@@ -51,7 +51,7 @@ async function RightSidebar() {
       <div className='flex flex-1 flex-col justify-start'>
         <h3 className='text-heading4-medium text-light-1'>Similar Minds</h3>
         <div className='mt-7 flex w-[350px] flex-col gap-10'>
-          {/* {similarMinds.users.length > 0 ? (
+          {similarMinds.users.length > 0 ? (
             <>
               {similarMinds.users.map((person) => (
                 <UserCard
@@ -66,7 +66,7 @@ async function RightSidebar() {
             </>
           ) : (
             <p className='!text-base-regular text-light-3'>No users yet</p>
-          )} */}
+          )}
         </div>
       </div>
     </section>
