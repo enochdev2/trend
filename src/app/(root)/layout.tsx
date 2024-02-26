efa25802d4cab311076806199b12691aab0af113
enoch-promise <enochpromise.va@gmail.com>
2024-02-25 23:13:59 -0800

    CommunityHub

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -11,8 +11,8 @@ import { dark } from "@clerk/themes";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Threads",
-  description: "A Next.js 13 Meta Threads application",
+  title: "CommunityHub",
+  description: "CommunityHub: A Collaborative Community Hu",
 };
 
 export default function RootLayout({
